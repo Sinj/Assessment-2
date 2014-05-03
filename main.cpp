6764089dc60a26ead64c3647d575a0e714c916b8
sinj <sinjun1107@aol.com>
2014-05-03 18:23:30 +0100

    added linklist

--- main.cpp
+++ main.cpp
@@ -19,6 +19,11 @@ bool File_has_read = true;
 
 int main(int argc, char* args[])
 {
+
+	//The images
+	SDL_Surface* hello = NULL;
+	SDL_Surface* screen = NULL;
+	int Blocksize = 0;
 	bool fine = false;
 	int startR = 0, startC = 0, goalR = 0, goalC = 0;
 	//M = Rows and N = columns in the image,
@@ -35,8 +40,19 @@ int main(int argc, char* args[])
 		//save layout to matrix
 		Matrix layout(M, N, inputf);
 
-		//layout.Output_Matrix(M, N);//outmatrix to chec that layout is saved
+		layout.Output_Matrix(M, N);//outmatrix to chec that layout is saved
+		do{
+		//get blocksize
+		cout << "Enter the block size of the screen, recomended 20, range 20-40\n";
+		cin >> Blocksize;
+		if (Blocksize > 19 && Blocksize < 41){
+			fine = true;
+			break;
+		}
+		cout << "Blocksize input is out of range\n";
 
+	} while (!fine);
+	fine = false;
 		//get start location		
 		do{
 			cout << "Enter start row location: \nRange 0-19\n";
@@ -66,6 +82,65 @@ int main(int argc, char* args[])
 		} while (!fine);
 		//Start SDL
 		SDL_Init(SDL_INIT_EVERYTHING);
+
+		//Set up screen
+		screen = SDL_SetVideoMode(N*Blocksize, M*Blocksize, 32, SDL_SWSURFACE);
+	
+		//Apply image to screen
+		SDL_BlitSurface(hello, NULL, screen, NULL);
+
+		//make and rec object
+		SDL_Rect Rectangle;
+		//set rec height
+		Rectangle.h = Blocksize;
+		//set rec weight
+		Rectangle.w = Blocksize;
+
+		//def a a colour
+		Uint32 White = SDL_MapRGB(screen->format, 255, 255, 255);
+		//hold increass value
+		double temi=0, temj = 0;
+
+		//outputs the layout of the text file onto the screen.
+		for (int i = 0; i < M; i++){
+			for (int j = 0; j < N; j++){
+				Rectangle.y = temi;
+				Rectangle.x = temj;
+				if (layout.get(i, j) == 0){
+					SDL_FillRect(screen, &Rectangle, White);
+					SDL_Flip(screen);
+				}
+				else{
+					NULL;
+				}
+				temj += Blocksize;
+			}
+			temj = 0;
+			temi += Blocksize;
+		}
+
+		//Update Screen
+		//SDL_Flip(screen);
+
+		//Pause
+		//SDL_Delay(2000);
+
+		//Free the loaded image
+		SDL_FreeSurface(hello);
+
+
+		SDL_Event event;
+		bool gameRunning = true;
+		while (gameRunning)
+		{
+			if (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT)
+				{
+					gameRunning = false;
+				}
+			}
+		}
 
 		//Quit SDL
 		SDL_Quit();
